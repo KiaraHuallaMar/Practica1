8efad79698b36eed47859852b76f5bab844c666e
GenesisLucero <148606419+GenesisLucero@users.noreply.github.com>
2024-09-18 16:48:28 -0500

    Actualizacion Grupo_ApoyoController.java

--- src/main/java/com/upc/trabajotf/projectbienestarcompany/controller/Grupo_ApoyoController.java
+++ src/main/java/com/upc/trabajotf/projectbienestarcompany/controller/Grupo_ApoyoController.java
@@ -1,56 +1,61 @@
 package com.upc.trabajotf.projectbienestarcompany.controller;
 
+import com.upc.trabajotf.projectbienestarcompany.dtos.Grupo_ApoyoDTO;
 import com.upc.trabajotf.projectbienestarcompany.entities.Grupo_Apoyo;
-import com.upc.trabajotf.projectbienestarcompany.service.Grupo_ApoyoService;
+import com.upc.trabajotf.projectbienestarcompany.serviceinterfaces.Grupo_ApoyoServiceInterfaces;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/grupos-apoyo")
 public class Grupo_ApoyoController {
 
     @Autowired
-    private Grupo_ApoyoService grupoApoyoService;
+    private Grupo_ApoyoServiceInterfaces grupoApoyoServiceInterfaces;
 
-    // Crear un nuevo grupo de apoyo
-    @PostMapping("/grupo")
-    public ResponseEntity<Grupo_Apoyo> createGrupoApoyo(@RequestBody Grupo_Apoyo grupoApoyo) {
-        Grupo_Apoyo newGroup = grupoApoyoService.createGrupoApoyo(grupoApoyo);
-        return ResponseEntity.ok(newGroup);
+    //Listar Grupo de Apoyo
+    @GetMapping
+    public List<Grupo_ApoyoDTO> obtenerGrupos(){
+
+        return grupoApoyoServiceInterfaces.listar().stream().map(x->{
+            ModelMapper m=new ModelMapper();
+            return m.map(x,Grupo_ApoyoDTO.class);
+        }).collect(Collectors.toList());
     }
 
-    // Obtener todos los grupos de apoyo
-    @GetMapping("/grupos")
-    public ResponseEntity<List<Grupo_Apoyo>> getAllGruposApoyo() {
-        List<Grupo_Apoyo> grupos = grupoApoyoService.getAllGruposApoyo();
-        return ResponseEntity.ok(grupos);
+    //Registrar Grupo de Apoyo
+    @PostMapping
+    public void registrar(@RequestBody Grupo_ApoyoDTO grupoApoyoDTO){
+        ModelMapper m = new ModelMapper();
+        Grupo_Apoyo grupoApoyo = m.map(grupoApoyoDTO, Grupo_Apoyo.class);
+        grupoApoyoServiceInterfaces.registrar(grupoApoyo);
     }
 
-    // Obtener un grupo de apoyo por su ID
-    @GetMapping("/grupo/{id}")
-    public ResponseEntity<Grupo_Apoyo> getGrupoApoyoById(@PathVariable int id) {
-        Optional<Grupo_Apoyo> grupo = grupoApoyoService.getGrupoApoyoById(id);
-        return grupo.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    //Listar por Id Grupo de Apoyo
+    @GetMapping("/{id}")
+    public Grupo_ApoyoDTO listarId(@PathVariable("id") Integer id){
+        ModelMapper m=new ModelMapper();
+        Grupo_ApoyoDTO grupoApoyoDTO=m.map(grupoApoyoServiceInterfaces.listarId(id),Grupo_ApoyoDTO.class);
+        return grupoApoyoDTO;
     }
 
-    // Actualizar un grupo de apoyo por su ID
-    @PutMapping("/grupo/{id}")
-    public ResponseEntity<Grupo_Apoyo> updateGrupoApoyo(@PathVariable int id, @RequestBody Grupo_Apoyo grupoApoyo) {
-        Optional<Grupo_Apoyo> updatedGrupo = grupoApoyoService.updateGrupoApoyo(id, grupoApoyo);
-        return updatedGrupo.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    //Actualizar Grupo de Apoyo
+    @PutMapping
+    public void actualizar(@RequestBody Grupo_ApoyoDTO grupoApoyoDTO){
+        ModelMapper m=new ModelMapper();
+        Grupo_Apoyo grupoApoyo=m.map(grupoApoyoDTO,Grupo_Apoyo.class);
+        grupoApoyoServiceInterfaces.actualizar(grupoApoyo);
     }
 
-    // Eliminar un grupo de apoyo por su ID
-    @DeleteMapping("/grupo/{id}")
-    public ResponseEntity<Void> deleteGrupoApoyo(@PathVariable int id) {
-        if (grupoApoyoService.deleteGrupoApoyo(id)) {
-            return ResponseEntity.ok().build();
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    //Eliminar Grupo de Apoyo
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id") Integer id){
+        grupoApoyoServiceInterfaces.eliminar(id);
     }
 }
+
