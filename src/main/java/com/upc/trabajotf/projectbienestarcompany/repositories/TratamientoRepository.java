c4135245f01a7b2eeeeea991b8ea3dad340684f7
Kiara Hualla Martínez <u20211d621@upc.edu.pe>
2024-09-18 16:54:56 -0500

    Update TratamientoRepository.java
    agregacion de query (Listar los tratamientos asignado por universitario)

--- src/main/java/com/upc/trabajotf/projectbienestarcompany/repositories/TratamientoRepository.java
+++ src/main/java/com/upc/trabajotf/projectbienestarcompany/repositories/TratamientoRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TratamientoRepository extends JpaRepository<Tratamiento,Integer> {
-}
+    @Query ("select t.nombre from Tratamiento t join Usuario u On t.usuario.id = u.id where u.id =:id and u.rol.id = 1")
+    List<Tratamiento> nombre (@Param("id")int id);
